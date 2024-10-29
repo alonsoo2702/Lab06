8b20e2e67c834a1bc7b6dc64c26b595d3a53e798
alonsoo2702 <138326803+alonsoo2702@users.noreply.github.com>
2024-10-28 19:14:16 -0500

    Create Main.java

---
+++ Ejercicio1/Main.java
@@ -0,0 +1,10 @@
+package ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        CarritoModelo modelo = new CarritoModelo();
+        CarritoVista vista = new CarritoVista();
+        CarritoControlador controlador = new CarritoControlador(modelo, vista);
+        controlador.iniciar();
+    }
+}
