c330d247b7101deeb18450aa142c7f25a6b58e66
alonsoo2702 <138326803+alonsoo2702@users.noreply.github.com>
2024-10-28 20:14:47 -0500

    Create InventarioModel.java

---
+++ Ejercicio3/InventarioModel.java
@@ -0,0 +1,30 @@
+package ejercicio3;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InventarioModel {
+    private List<Item> items;
+    
+    public InventarioModel() {
+        items = new ArrayList<>();
+    }
+    
+    public void agregarItem(Item item) {
+        items.add(item);
+    }
+    
+    public void eliminarItem(Item item) {
+        items.remove(item);
+    }
+    
+    public List<Item> obtenerItems() {
+        return new ArrayList<>(items);
+    }
+    
+    public Item buscarItem(String nombre) {
+        return items.stream()
+                   .filter(item -> item.getNombre().equalsIgnoreCase(nombre))
+                   .findFirst()
+                   .orElse(null);
+    }
+}
