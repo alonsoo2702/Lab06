5d5992cce4fc5e4c3f84fb124316e29f1bb6a563
alonsoo2702 <138326803+alonsoo2702@users.noreply.github.com>
2024-10-28 20:14:58 -0500

    Create InventarioView.java

---
+++ Ejercicio3/InventarioView.java
@@ -0,0 +1,23 @@
+package ejercicio3;
+import java.util.List;
+
+public class InventarioView {
+    public void mostrarInventario(List<Item> items) {
+        System.out.println("\n=== Inventario Actual ===");
+        for (Item item : items) {
+            mostrarDetallesItem(item);
+        }
+    }
+    
+    public void mostrarDetallesItem(Item item) {
+        System.out.println("\nDetalles del Item:");
+        System.out.println("Nombre: " + item.getNombre());
+        System.out.println("Cantidad: " + item.getCantidad());
+        System.out.println("Tipo: " + item.getTipo());
+        System.out.println("Descripción: " + item.getDescripcion());
+    }
+    
+    public void mostrarMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
+}
