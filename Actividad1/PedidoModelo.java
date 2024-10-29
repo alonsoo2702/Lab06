7d671202a9ba39ee5da743f510e0a6c67af06515
alonsoo2702 <138326803+alonsoo2702@users.noreply.github.com>
2024-10-28 19:09:52 -0500

    Create PedidoModelo.java

---
+++ Actividad1/PedidoModelo.java
@@ -0,0 +1,16 @@
+package actividad1;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PedidoModelo {
+	private List<Pedido> pedidos;
+	public PedidoModelo() {
+		pedidos = new ArrayList<>();
+	}
+	public void agregarPedido(Pedido pedido) {
+		pedidos.add(pedido);
+	}
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+}
