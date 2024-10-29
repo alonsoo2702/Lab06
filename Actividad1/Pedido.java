21d724f0d3f5f92cc0e892942906f1248ec6e284
alonsoo2702 <138326803+alonsoo2702@users.noreply.github.com>
2024-10-28 19:09:38 -0500

    Create Pedido.java

---
+++ Actividad1/Pedido.java
@@ -0,0 +1,11 @@
+package actividad1;
+
+public class Pedido {
+	 private String nombrePlato;
+	 public Pedido(String nombrePlato) {
+	 this.nombrePlato = nombrePlato;
+	 }
+	 public String getNombrePlato() {
+	 return nombrePlato;
+	 }
+}
